41c41d86516f7624a1d90f549a75c79c0bb65e05
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-08-22 22:51:43 +0500

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+import Hero from "@/components/hero";
+import Dialog from "@/components/dialog";
+
+
+export default function Home() {
+  return (
+    <div className="bg-gray-800 text-white h-screen justify-center">
+      <h1></h1>
+    
+      <Hero />
+
+
+    </div>
+  );
+}
---
+++ src/components/dialog.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog"
+
+const Dialog = () => {
+  return (
+    <AlertDialog >
+    <AlertDialogTrigger ><h1  className=" flex p-5
+     ">HELLO</h1></AlertDialogTrigger>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+        <AlertDialogDescription>
+          This action cannot be undone. This will permanently delete your account
+          and remove your data from our servers.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction>Continue</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+  )
+}
+
+export default Dialog
